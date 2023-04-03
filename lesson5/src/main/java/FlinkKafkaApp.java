fc2a97082a5b20cfe7eb97c52ea73dd833529a96
my-name <alexeydruzik@gmail.com>
2023-04-03 09:22:02 +0300

    Added example of transformation.

--- lesson5/src/main/java/FlinkKafkaApp.java
+++ lesson5/src/main/java/FlinkKafkaApp.java
@@ -1,10 +1,12 @@
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 public class FlinkKafkaApp {
 
@@ -19,10 +21,22 @@ public class FlinkKafkaApp {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
         DataStream<String> lines = env.fromSource(source, WatermarkStrategy.noWatermarks(), "sentences");
-        lines.print();
-        env.execute("FlinkKafka");
 
+        DataStream<String> modifiedStream = lines.flatMap(new TopicParser());
+        modifiedStream.print();
+        env.execute("FlinkKafkaApp");
         //DataStream<String> orderA = env.fromCollection(Arrays.asList("new", "new3"));
         //orderA.print();
     }
+
+        public static class TopicParser implements FlatMapFunction<String, String> {
+        @Override
+        public void flatMap(String value, Collector<String> out) {
+            //out.collect(value + "_");
+            for (String token : value.split("\\W")) {
+                out.collect(token + "_");
+            }
+        }
+    }
+
 }
