3221b6bb3dd3411b105dff4a2e9b903afaab8bf5
my-name <alexeydruzik@gmail.com>
2023-04-09 14:33:50 +0300

    Fixed enum.

--- lesson5/src/main/java/dto/Gender.java
+++ lesson5/src/main/java/dto/Gender.java
@@ -1,12 +1,6 @@
 package dto;
 
 public enum Gender {
-        MALE("MALE"),
-        FEMALE("FEMALE");
-
-        private final String name;
-
-        Gender(String name) {
-          this.name = name;
-        }
+        MALE,
+        FEMALE
 }
